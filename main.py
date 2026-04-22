355182b8c6a6b8e0a23e2f673345d1bfd566fcc5
skirk <semasosnov1@gmail.com>
2026-04-22 04:05:07 +0300

    New data types have been added for table columns

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ class GenerateRequest(BaseModel):
 def generate(request: GenerateRequest):
     rows = request.rows
     schema = request.schema
-    headers = [field.name for field in request.schema]
+    headers = [field.name for field in schema]
     data = []
     for i in range(rows):
         row = []
@@ -25,12 +25,22 @@ def generate(request: GenerateRequest):
             fieldType = field.type
             if fieldType == "full_name":
                 value = fake.name()
+            elif fieldType == "job":
+                value = fake.job()
+            elif fieldType == "street":
+                value = fake.street_address()
+            elif fieldType == "city":
+                value = fake.city()
+            elif fieldType == "company":
+                value = fake.company()
+            elif fieldType == "birth_date":
+                value = fake.date_of_birth(minimum_age = 18, maximum_age = 80).isoformat()
             elif fieldType == "email":
                 value = fake.email()
             elif fieldType == "phone":
                 value = fake.phone_number()
             else:
-                value = f"unknown_{fieldType}"
+                value = f"unknown_type_name = {fieldType}"
             row.append(value)
         data.append(row)
     return {"headers":headers, "data":data}
